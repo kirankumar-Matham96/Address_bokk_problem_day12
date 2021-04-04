32fd819823a1b438a1ba07534ae0a98ee321ab93
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-04 21:33:50 +0530

    ADD: Created address book and added contact to it

--- src/main/java/comAddresBook/AddressBook.java
+++ src/main/java/comAddresBook/AddressBook.java
@@ -1,9 +1,30 @@
 package comAddresBook;
 
+import java.util.ArrayList;
+
 public class AddressBook
 {
+    ArrayList<Contact> al;
+
+    //method to add contact to a list
+    public void addToList(Contact contact)
+    {
+        al = new ArrayList<>();
+        al.add(contact);
+    }
+
+    //method to get contact details
+    public void getContact()
+    {
+        System.out.println(al.get(0));
+    }
+
+    //main method
     public static void main(String[] args)
     {
-        System.out.println("Welcome to AddressBook Program!");
+        Contact member = new Contact("Hello","There","Here it is","famous city","new state",543549,9848307659L,"hello@gmail.com");
+        AddressBook ab = new AddressBook();
+        ab.addToList(member);
+        ab.getContact();
     }
 }
---
+++ src/main/java/comAddresBook/Contact.java
@@ -0,0 +1,40 @@
+package comAddresBook;
+
+public class Contact
+{
+    public final String FIRST_NAME;
+    public final String LAST_NAME;
+    public final String ADDRESS;
+    public final String CITY;
+    public final String STATE;
+    public final int ZIP;
+    public final long PHONE_NUMBER;
+    public final String EMAIL;
+
+    public Contact(String first_name, String last_name, String address, String city, String state, int zip, long phone_number, String email)
+    {
+        FIRST_NAME = first_name;
+        LAST_NAME = last_name;
+        ADDRESS = address;
+        CITY = city;
+        STATE = state;
+        ZIP = zip;
+        PHONE_NUMBER = phone_number;
+        EMAIL = email;
+    }
+
+    @Override//to get details
+    public String toString() {
+        return "Contact{" +"\n"+
+                "FIRST_NAME ='" + FIRST_NAME + '\'' +
+                ","+"\n" +"LAST_NAME ='" + LAST_NAME + '\'' +
+                ","+"\n"+"ADDRESS ='" + ADDRESS + '\'' +
+                ","+"\n"+"CITY ='" + CITY + '\'' +
+                ","+"\n"+"STATE ='" + STATE + '\'' +
+                ","+"\n"+"ZIP =" + ZIP +
+                ","+"\n"+"PHONE_NUMBER =" + PHONE_NUMBER +
+                ","+"\n"+"EMAIL ='" + EMAIL + '\'' +"\n"+
+                '}';
+    }
+
+}
